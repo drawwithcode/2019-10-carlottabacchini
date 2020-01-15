360fd539557a6f4458a2b01cace0abd124773d5e
Carlotta Bacchini <carlotta@MacBook-Pro-di-Carlotta-2.local>
2020-01-15 16:36:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,32 @@
-// Initializing variables for creategraphics
+// Declaring variables
 var myModel;
 var myFont;
 var cam;
-var cam2
-var currentCamera;
+
+// variables for array
 var dollars = [];
 var numDollars = 3000;
-var tex;
+var tex; // texture
 
-let locX, locY;
+let locX, locY; // mouse variables
 
 function preload() {
-
   myModel = loadModel("assets/2.obj", true);
   myFont = loadFont('assets/RobotoMono-Bold.ttf')
   tex = loadImage('assets/tex.png')
-
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
-  cam = createCamera();
-
-  // set variable for previously active camera:
-  currentCamera = 0
+  cam = createCamera(); // creating a P5.JS Camera for Webgl
 
   angleMode(DEGREES)
   frameRate(50)
 
-  for (var i = 0; i < numDollars; i++) {
-    dollars.push(new Dollar(random(-800, 800), random(-800, 800), random(1000, 500), random(0, PI), random(10, 28), random(0.03, 10)));
+
+  for (var i = 0; i < numDollars; i++) { // generate the object
+    dollars.push(new Dollar(random(-900, 900), random(-900, 900), random(900, 600), random(10, 28), random(0.03, 10))); // parameters
   }
 
 }
@@ -38,31 +34,32 @@ function setup() {
 function draw() {
   background(0);
 
+  // setting mouse variables
   locX = mouseX - width / 2;
   locY = mouseY - height / 2;
 
+  // setting camera
   setCamera(cam)
   cam.lookAt(0, 0, 0);
-  cam.setPosition(100, 100, -600 * frameCount / 200)
+  cam.setPosition(100, 100, -600 * frameCount / 200) // camera animation for intro
 
+  // setting that the intro finish after 200 frame
   if (frameCount >= 200) {
     cam.setPosition(100, 100, -600)
   }
-  // if (frameCount >= 210) {           // if you want to move the 3D model after the intro
+
+  // if you want to move the 3D model after the intro
+  // if (frameCount >= 210) {
   //   cam.setPosition(locX,locY, -600)
   // }
 
-  if (keyIsDown(ENTER) && frameCount >= 200) {
-    setCamera(cam2)
-    currentCamera = 1
-  }
 
   for (var i = 0; i < dollars.length; i++) {
-    dollars[i].move();
-    dollars[i].display();
+    dollars[i].move(); // call the function that allows the movement of the object
+    dollars[i].display(); // call the function that display the object
   }
 
-
+  // text
   push()
   fill('#c5ff8c');
   textFont(myFont);
@@ -75,6 +72,7 @@ function draw() {
   pop()
 
 
+  // my model
   push()
   rotateZ(180)
   rotateY(180)
@@ -87,45 +85,41 @@ function draw() {
 
   pop()
 
-
-
 }
 
+// set the parameters of the class dollars
 class Dollar {
-  constructor(xPos, yPos, zPos, incl, sizeX, delta) {
+  constructor(xPos, yPos, zPos, sizeX, delta) {
 
     this.x = xPos;
     this.y = yPos;
     this.z = zPos;
-    this.angle = incl;
     this.lenght = sizeX;
     this.hight = sizeX / 2.35;
     this.speed = delta;
   }
 
-  move() {
+  move() { // setting the movement from the back to the front
     this.z -= this.speed;
   };
 
-  display() {
+
+  display() { // setting the aspect of the array of dollars 
 
     push();
     translate(this.x, this.y, this.z);
-    rotateX(this.angle);
-    rotateY(this.angle / 4);
-    rotateZ(this.angle / 3);
     noStroke();
     pointLight(159, 220, 141, locX, -locY, 0);
     pointLight(240, 48, 104, -locX, locY, 0);
     texture(tex)
     plane(this.lenght, this.hight);
-
     pop();
 
   };
 
 }
 
+// function that allows to resize the windows
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
